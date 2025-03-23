7449afcbb573c608a5e6ba3fef9763ffa12e12d1
BaliaSahu <baliasahu7382@gmail.com>
2025-03-23 08:47:13 +0530

    Update Constants.js

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,4 @@
-// export const BASE_URL="http://localhost:7000";
+export const BASE_URL="http://localhost:7000";
 // export const BASE_URL="https://devtinder-backend-kg3v.onrender.com"
-export const BASE_URL="https://devtinder-backend-1-gxoa.onrender.com"
+// export const BASE_URL="https://devtinder-backend-1-gxoa.onrender.com"
 
