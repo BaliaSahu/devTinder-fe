d553d1fdd04ff7948b1e23512558c56d18ee8acf
BaliaSahu <baliasahu7382@gmail.com>
2025-03-23 10:44:13 +0530

    Update Pm.jsx

--- src/components/Pm.jsx
+++ src/components/Pm.jsx
@@ -5,7 +5,7 @@ import axios from "axios"
 function Pm() {
       const handleBuyClick=async(type)=>{
             console.log(type)
-            console.log(document.cookie);
+            console.log(document.cookie,"hihhi");
             const order=await axios.post(BASE_URL+"/payment/createorders",
                   {type:type},
                   {withCredentials:true}
